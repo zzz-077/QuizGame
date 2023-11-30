78f989fdec88dff2aa5b947c5bccdd344e556fa8
zzz077 <122636696+zzz-077@users.noreply.github.com>
2023-11-30 23:58:30 +0400

    comm1

---
+++ QuizGame/content/app.js
@@ -0,0 +1,6 @@
+const startUp_btn = document.querySelector(".bottom_content button");
+
+startUp_btn.addEventListener("click", () => {
+    const InagamePage = "game.html";
+    window.location.href = InagamePage;
+});
