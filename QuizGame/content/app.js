a5cb70de663a0eaacdefdce2277a4474221f8a44
zzz077 <122636696+zzz-077@users.noreply.github.com>
2023-12-07 00:56:31 +0400

    comm7

--- QuizGame/content/app.js
+++ QuizGame/content/app.js
@@ -1,3 +1,6 @@
+//===========================
+//==========DOM==============
+//===========================
 const page = document.body.id;
 const startUp_btn = document.querySelector(".bottom_content button");
 const startUp_content = document.querySelector(".startUp_content");
@@ -17,20 +20,109 @@ const lose_retry_button = document.querySelector(".lose_retry_button button");
 const LiveScore_box = document.querySelector(".score_box h1");
 const AlertScore_box = document.querySelector(".lose_score div strong");
 const MenuMax_score = document.querySelector(".max_score h3");
+//=================================
+//==========VARIABLES==============
+//=================================
 let InagamePage;
 let width = 16 * 100;
 let IntervalX;
 let score = 0;
 let Maxscore = 0;
+let AnswerIndex;
 timeCheck = false;
+let mouseX;
+let mouseY;
+let lobbySound;
+let ingameSound;
+let WrongAns;
+let CorrectAns;
+//=====================================
+//==========CURSOR EFFect==============
+//=====================================
+const colors = ["#963fc1", "#a8e7fe", "#90d1f5", "#cbf1ba"];
+function randColor() {
+    let pickedColorIndex = Math.floor(Math.random() * colors.length + 1);
+    let pickedColor = colors[pickedColorIndex];
+    return pickedColor;
+}
+window.onmousemove = (e) => {
+    // const dot = document.createElement("div");
+    const dot = document.createElement("i");
+    dot.classList.add("dot", "fas", "fa-star");
+    mouseX = e.clientX;
+    mouseY = e.clientY;
 
-document.addEventListener("DOMContentLoaded", () => {
-    Maxscore = parseInt(localStorage.getItem("Maxscore")) || 0;
-    if (MenuMax_score) {
-        MenuMax_score.innerHTML = Maxscore;
+    dot.style.transform = `translate(${mouseX}px,${mouseY}px)`;
+
+    dot.style.color = randColor();
+
+    document.body.appendChild(dot);
+
+    setInterval(() => {
+        if (document.body.contains(dot)) {
+            document.body.removeChild(dot);
+        }
+    }, 200);
+};
+
+//=====================================
+//==========SOUND EFFECTS==============
+//=====================================
+function playMainMusic() {
+    lobbySound = new Audio("../Sounds/mainsound.mp3");
+    lobbySound.volume = 0.2;
+
+    lobbySound.addEventListener(
+        "ended",
+        function () {
+            this.currentTime = 0;
+            this.play();
+        },
+        false
+    );
+    lobbySound.play();
+}
+function playInGameMusic() {
+    ingameSound = new Audio("../Sounds/Ingamesound.mp3");
+    ingameSound.volume = 0.2;
+
+    ingameSound.addEventListener(
+        "ended",
+        function () {
+            this.currentTime = 0;
+            this.play();
+        },
+        false
+    );
+
+    ingameSound.play();
+}
+function playInGameMusicStop() {
+    if (ingameSound) {
+        ingameSound.pause();
+        ingameSound.currentTime = 0;
     }
-});
+}
+function WrongAnswer() {
+    WrongAns = new Audio("../Sounds/WrongAnswersound.mp3");
+    WrongAns.play();
+}
+function CorrectAnswer() {
+    CorrectAns = new Audio("../Sounds/CorrectAnswersound.mp3");
+    CorrectAns.play();
+}
 
+// window.addEventListener("DOMContentLoaded", (event) => {
+//     const audio = document.querySelector("audio");
+//     audio.volume = 0.2;
+//     audio.play();
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+// });
+//===================================
+//==========PROGRESSBAR==============
+//===================================
 function progresBarFunc() {
     IntervalX = setInterval(() => {
         if (width == 0) {
@@ -49,10 +141,36 @@ function progresBarFunc() {
     }, 100);
 }
 
+function timeLimitFunc() {
+    if (timeCheck == true) {
+        AlertScore_box.innerHTML = score;
+        setTimeout(() => {
+            gamePageLoadRemove();
+        }, 100);
+        playInGameMusicStop();
+        WrongAnswer();
+        lose_box.classList.add("active");
+        lose_content.classList.add("active");
+    }
+}
+//===================================
+//==========LOCALSTORAGE=============
+//===================================
+document.addEventListener("DOMContentLoaded", () => {
+    Maxscore = parseInt(localStorage.getItem("Maxscore")) || 0;
+    if (MenuMax_score) {
+        MenuMax_score.innerHTML = Maxscore;
+    }
+});
+//======================================
+//==========CHANGELOCATION==============
+//======================================
+
 function changelocToGame() {
     const InagamePage = "game.html";
     window.location.href = InagamePage;
 }
+
 function changelocToHome() {
     score = 0;
     const InagamePage = "index.html";
@@ -66,20 +184,9 @@ function gamePageLoadRemove() {
     game_content.classList.remove("active");
 }
 
-function timeLimitFunc() {
-    if (timeCheck == true) {
-        AlertScore_box.innerHTML = score;
-        setTimeout(() => {
-            gamePageLoadRemove();
-        }, 100);
-
-        lose_box.classList.add("active");
-        lose_content.classList.add("active");
-    }
-}
-
 switch (page) {
     case "main_board":
+        playMainMusic();
         startUp_btn.addEventListener("click", startClick);
         function startClick() {
             startUp_content.classList.add("active");
@@ -87,11 +194,11 @@ switch (page) {
                 changelocToGame();
             }, 300);
         }
-
         break;
     case "game_board":
+        playInGameMusic();
+
         lose_retry_button.addEventListener("click", retrygameClick);
-        let AnswerIndex;
 
         function retrygameClick() {
             if (score > Maxscore) {
@@ -185,10 +292,12 @@ switch (page) {
                             progressBar.style.backgroundColor =
                                 "rgba(217, 186, 8, 1)";
                             clearInterval(IntervalX);
-
+                            CorrectAnswer();
                             return returnAgain();
                         } else {
                             AlertScore_box.innerHTML = score;
+                            playInGameMusicStop();
+                            WrongAnswer();
                             setTimeout(() => {
                                 gamePageLoadRemove();
                             }, 100);
